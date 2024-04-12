03c00d8f18c84384149119aea7c9e92053430d4d
Anastasiia Marchenko <38503936+dots13@users.noreply.github.com>
2024-04-12 02:56:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -27,12 +27,12 @@ def main():
     lda_model = model_init('lda')
     model = model_init('mdfend')
 
-    client = OpenAI(temperature=0)
+    #client = OpenAI(temperature=0)
     # Initialize agents
-    class_agent = ClassAgent(client=client)
-    headline_agent = HeadlineAgent(client=client)
-    filter_agent = FilterAgent(client=client)
-    decision_agent = DecisionAgent(client=client)
+    #class_agent = ClassAgent(client=client)
+    #headline_agent = HeadlineAgent(client=client)
+    #filter_agent = FilterAgent(client=client)
+    #decision_agent = DecisionAgent(client=client)
 
 
     # Streamlit UI Code
@@ -40,10 +40,10 @@ def main():
     st.title("El Salvador")
 
     # Sample input for demonstration
-    headline = st.text_input("Titular", "")
+    #headline = st.text_input("Titular", "")
     news_text = st.text_area("Cuerpo de la Noticia", "")
 
-    headline = limit_tokens(headline)
+    #headline = limit_tokens(headline)
     news = limit_tokens(news_text)
 
     if st.button('Process'):
@@ -51,34 +51,34 @@ def main():
         news_text = news_text.replace("\\n", " ")
         lda_label = lda_model.predict_topic(news_text)
         result_pred_proba = model.predict(news_text, lda_label)
-        st.write(lda_label)
+        #st.write(lda_label)
         st.write(f"The news is Fake with probability: {100*result_pred_proba:.2f} %")
-        class_result = class_agent.run_class_agent(headline=headline)
-        st.write("Class result:", class_result)  # Modification here
-        data = json.loads(class_result)
-        subject = data["subject"]
-        event = data["event"]
-        # Info Extraction
-        context = info_extraction(headline, serper_ai_key)
-        st.write("Contexto-Fuentes utilizadas:", context)  # Modification here
-
-        # Headline Alignment
-        alignment_result = headline_agent.analyze_alignment(headline=headline, news=news)
-        st.write("Alineamiento Titular-Noticia:", alignment_result)  # Modification here
-        data_alignment = json.loads(alignment_result)
-        alignment_label = data_alignment["label"]
-
-        # Misinformation Campaign Filter
-        filter_result = filter_agent.run_filter_agent(headline, context)
-        st.write("Difusion del Titular (times)", filter_result)  # Modification here
-        filter_data = json.loads(filter_result)
-        times = filter_data["times"]
-
-        # Decision Making Agent
-        decision_result = decision_agent.run_decision_agent(news, context, (1-result_pred_proba), alignment_label, times)
-
-        # Final decision display
-        st.write("Decision Final", decision_result)  # Modification here
+        #class_result = class_agent.run_class_agent(headline=headline)
+        #st.write("Class result:", class_result)  # Modification here
+        #data = json.loads(class_result)
+        #subject = data["subject"]
+        #event = data["event"]
+        ## Info Extraction
+        #context = info_extraction(headline, serper_ai_key)
+        #st.write("Contexto-Fuentes utilizadas:", context)  # Modification here
+		#
+        ## Headline Alignment
+        #alignment_result = headline_agent.analyze_alignment(headline=headline, news=news)
+        #st.write("Alineamiento Titular-Noticia:", alignment_result)  # Modification here
+        #data_alignment = json.loads(alignment_result)
+        #alignment_label = data_alignment["label"]
+		#
+        ## Misinformation Campaign Filter
+        #filter_result = filter_agent.run_filter_agent(headline, context)
+        #st.write("Difusion del Titular (times)", filter_result)  # Modification here
+        #filter_data = json.loads(filter_result)
+        #times = filter_data["times"]
+		#
+        ## Decision Making Agent
+        #decision_result = decision_agent.run_decision_agent(news, context, (1-result_pred_proba), alignment_label, times)
+		#
+        ## Final decision display
+        #st.write("Decision Final", decision_result)  # Modification here
 
 
 if __name__ == '__main__':
